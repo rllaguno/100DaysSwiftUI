96d27428eb891d7e037b09fb0786c56c22cbb13b
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-07-03 02:09:55 -0500

    Project1 Part1

---
+++ projects/project1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rodrigo Llaguno on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
