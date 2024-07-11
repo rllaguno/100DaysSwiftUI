311a843d3110f6f8e074974cc87037130ab9648e
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-07-11 12:18:03 -0600

    Project4 Part1

---
+++ projects/project4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rodrigo Llaguno on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
