f00d9b0720f44240dfa6af50aa170b9a8775f1e8
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-07-17 12:24:05 -0600

    Project6 Part1

---
+++ projects/project6/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Rodrigo Llaguno on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projects/project6/Animation/Animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Rodrigo Llaguno on 17/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
