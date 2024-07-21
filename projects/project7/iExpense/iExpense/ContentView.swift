d550fac0cecc751408b8746f13c9d81d70c3c746
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-07-21 15:37:57 -0600

    Project7 Part1

---
+++ projects/project7/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Rodrigo Llaguno on 21/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projects/project7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Rodrigo Llaguno on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
