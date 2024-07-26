dc6e470e6a7e6f4e1e3b99e0da56fa81ae634483
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-07-26 16:03:24 -0600

    Project8 Part2

---
+++ projects/project8/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Rodrigo Llaguno on 26/07/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
