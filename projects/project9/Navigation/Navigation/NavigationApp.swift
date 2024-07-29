57dbfee8ab97e58cb04e8b316b02b29bf9610f7c
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-07-29 13:40:20 -0600

    Project9 Part1

---
+++ projects/project9/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Rodrigo Llaguno on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
