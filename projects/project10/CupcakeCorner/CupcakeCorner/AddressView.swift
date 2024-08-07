3d30d440601bdf77e4e1e8ccedd614a99a02b095
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-08-06 18:43:14 -0600

    Project10 Part2

---
+++ projects/project10/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Rodrigo Llaguno on 06/08/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
