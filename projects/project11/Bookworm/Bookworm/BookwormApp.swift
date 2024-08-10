0a5fa4e7a4621fd2f02940e61c0fc8259de5f9b5
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-08-09 18:23:51 -0600

    Project11 Part1

---
+++ projects/project11/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Rodrigo Llaguno on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
