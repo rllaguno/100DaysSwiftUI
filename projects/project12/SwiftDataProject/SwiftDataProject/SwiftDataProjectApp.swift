e15e5b3042037a0c364788dadad0d5af19b67630
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-08-13 17:06:55 -0600

    Project12 Part1

---
+++ projects/project12/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Rodrigo Llaguno on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
