70fe2a63aae703fb5053ce5601828a5c2596efb8
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-08-18 15:02:57 -0600

    Project13 Part1

---
+++ projects/project13/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Rodrigo Llaguno on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
