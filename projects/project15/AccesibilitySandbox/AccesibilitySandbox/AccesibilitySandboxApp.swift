2d6cfd798709eaf7e8175fb26a9eca271246a285
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-08-30 12:24:01 -0600

    Project15 Part1

---
+++ projects/project15/AccesibilitySandbox/AccesibilitySandbox/AccesibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesibilitySandboxApp.swift
+//  AccesibilitySandbox
+//
+//  Created by Rodrigo Llaguno on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AccesibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
