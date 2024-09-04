65150610462926dac0a09ffc45a97553fd0122db
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-09-04 17:29:40 -0600

    Project16 Part1

---
+++ projects/project16/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Rodrigo Llaguno on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
