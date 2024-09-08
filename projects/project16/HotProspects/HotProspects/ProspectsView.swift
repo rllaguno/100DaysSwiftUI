a90ded1ee4b3f896cf3acf3798a406334d8360f4
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-09-08 14:32:12 -0600

    Project16 Part5

--- projects/project16/HotProspects/HotProspects/ProspectsView.swift
+++ projects/project16/HotProspects/HotProspects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Rodrigo Llaguno on 07/09/24.
 //
 
+import CodeScanner
 import SwiftData
 import SwiftUI
 
@@ -15,6 +16,8 @@ struct ProspectsView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @State private var isShowingScanner = false
+    @State private var selectedProspects = Set<Prospect>()
     
     let filter: FilterType
     var title: String {
@@ -30,7 +33,7 @@ struct ProspectsView: View {
     
     var body: some View {
         NavigationStack {
-            List(prospects) { prospect in
+            List(prospects, selection: $selectedProspects) { prospect in
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
@@ -38,13 +41,45 @@ struct ProspectsView: View {
                     Text(prospect.emailAddress)
                         .foregroundStyle(.secondary)
                 }
+                .swipeActions {
+                    Button("Delete", systemImage: "trash", role: .destructive) {
+                        modelContext.delete(prospect)
+                    }
+                    
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+                    } else {
+                        Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+                    }
+                }
+                .tag(prospect)
             }
                 .navigationTitle(title)
                 .toolbar {
-                    Button("Scan", systemImage: "qrcode.viewfinder") {
-                        let prospect = Prospect(name: "Paul Hudson", emailAddress: "email", isContacted: false)
-                        modelContext.insert(prospect)
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Scan", systemImage: "qrcode.viewfinder") {
+                            isShowingScanner = true
+                        }
                     }
+                    
+                    ToolbarItem(placement: .topBarLeading) {
+                        EditButton()
+                    }
+                    
+                    if selectedProspects.isEmpty == false {
+                        ToolbarItem(placement: .bottomBar) {
+                            Button("Delete Selected", action: delete)
+                        }
+                    }
+                }
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com",completion: handleScan)
                 }
         }
     }
@@ -58,6 +93,28 @@ struct ProspectsView: View {
             _prospects = Query(filter: #Predicate {
                 $0.isContacted == showContactedOnly
             }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+            modelContext.insert(person)
+            
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
         }
     }
 }
