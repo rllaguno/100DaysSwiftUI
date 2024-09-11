652f4b3d2f7fc8b5e7178451bf512bcc86636cb2
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-09-11 11:47:50 -0600

    Project17 Part1

---
+++ projects/project17/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Rodrigo Llaguno on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
